e57d6824a6192b56105f4e78c0e19775b2fa42a0
flycal6 <brianraythomas@gmail.com>
2017-05-08 08:31:49 +0300

    static practice

---
+++ StaticTest.java
@@ -0,0 +1,27 @@
+class StaticSuper {
+
+    static {
+        System.out.println("super static block");
+    }
+
+    StaticSuper() {
+        System.out.println("super constructor");
+    }
+}
+
+public class StaticTest extends StaticSuper {
+    static int rand;
+
+    static {
+        rand = (int) (Math.random()*6);
+        System.out.println("static block " + rand);
+    }
+
+    StaticTest() {
+        System.out.println("subclass constructor");
+    }
+    public static void main(String[] args) {
+        System.out.println("in main");
+        StaticTest st = new StaticTest();
+    }
+}
