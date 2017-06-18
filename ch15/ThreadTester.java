d98b1e470bf9700e33c90517b05d07dd85456d83
flycal6 <brianraythomas@gmail.com>
2017-06-18 13:35:59 +0300

    thread tester added to see order of stack calls when multiple threads exist

---
+++ ch15/ThreadTester.java
@@ -0,0 +1,30 @@
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("run() started");
+        go();
+        System.out.println("run() finished");
+    }
+
+    public void go() {
+        System.out.println("go() started");
+        doMore();
+        System.out.println("go() finished");
+    }
+
+    public void doMore() {
+        System.out.println("top o' the stack");
+    }
+}
+
+class ThreadTester {
+    public static void main(String[] args) {
+        Runnable threadJob = new MyRunnable();
+        Thread myThread = new Thread(threadJob);
+
+        System.out.println("calling thread.start()");
+
+        myThread.start();
+
+        System.out.println("back in main()");
+    }
+}
